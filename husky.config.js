55234b7c9cb1f223a0099a7d7433734608fd4d9e
Anton Kudryavtsev <boblobl4@gmail.com>
2020-03-13 17:24:27 +0300

    chore(project): commitlint integration
    Proper commit linting, especially important for semantic-release

--- husky.config.js
+++ husky.config.js
@@ -3,5 +3,6 @@ const tasks = arr => arr.join(" && ");
 module.exports = {
   hooks: {
     "pre-commit": tasks(["npm run test", "npm run docgen", "git add docs", "lint-staged"]),
+    "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
   },
 };
