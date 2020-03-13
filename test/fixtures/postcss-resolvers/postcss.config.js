8a7f9d48c9d6f02b08a71538f669d8802c8f55e3
Anton Kudryavtsev <boblobl4@gmail.com>
2020-03-13 07:33:59 +0300

    +1

--- test/fixtures/postcss-resolvers/postcss.config.js
+++ test/fixtures/postcss-resolvers/postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
     "postcss-import": {},
-    "postcss-url": { url: "copy", assetsPath: "images", useHash: "false" },
+    "postcss-url": { url: "copy", assetsPath: "images", useHash: false },
   },
 };
