ce004b53165e69026a0fb4c6d720ee214de399e7
Anton Kudryavtsev <boblobl4@gmail.com>
2020-03-13 07:37:58 +0300

    Test on commit

--- husky.config.js
+++ husky.config.js
@@ -2,6 +2,6 @@ const tasks = arr => arr.join(" && ");
 
 module.exports = {
   hooks: {
-    "pre-commit": tasks(["npm run docgen", "git add docs", "lint-staged"]),
+    "pre-commit": tasks(["npm run test", "npm run docgen", "git add docs", "lint-staged"]),
   },
 };
