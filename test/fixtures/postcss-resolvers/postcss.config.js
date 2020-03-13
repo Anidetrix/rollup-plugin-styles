bc33e9ed3e5dfd4d1e06cd26468cdcf389bbbef7
Anton Kudryavtsev <boblobl4@gmail.com>
2020-03-13 07:32:08 +0300

    Fix postcss-resolver test

--- test/fixtures/postcss-resolvers/postcss.config.js
+++ test/fixtures/postcss-resolvers/postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
     "postcss-import": {},
-    "postcss-url": { url: "copy", assetsPath: "images", useHash: "true" },
+    "postcss-url": { url: "copy", assetsPath: "images", useHash: "false" },
   },
 };
