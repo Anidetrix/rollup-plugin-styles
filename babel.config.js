3fb9f802d4d2acea67bb68b8e875142daab6ba80
Anton Kudryavtsev <boblobl4@gmail.com>
2020-03-15 00:48:29 +0300

    fix(node): wrong supported version of Node.js
    Was 8, but it failed on 8, 10 is minimal

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = api => {
   api.cache.invalidate(() => process.env.NODE_ENV === "production");
 
-  const presets = [["@babel/preset-env", { targets: { node: "8" } }]];
+  const presets = [["@babel/preset-env", { targets: { node: "10" } }]];
   const plugins = [];
 
   if (api.env("test")) presets[0][1].modules = "commonjs";
