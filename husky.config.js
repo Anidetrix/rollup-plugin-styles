1d4d53354221e17ba20facd22fc60e20012f64da
Anton Kudryavtsev <boblobl4@gmail.com>
2020-03-18 04:25:13 +0300

    build(docs): disable automatic docs generation on commit

--- husky.config.js
+++ husky.config.js
@@ -2,7 +2,7 @@ const tasks = arr => arr.join(" && ");
 
 module.exports = {
   hooks: {
-    "pre-commit": tasks(["npm run test", "npm run docgen", "git add docs", "lint-staged"]),
+    "pre-commit": tasks(["npm run test", "lint-staged"]),
     "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
   },
 };
