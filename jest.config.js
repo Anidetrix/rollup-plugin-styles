5e17b8d8d63126ae3e92dab31399b7aa3dd3722f
Anton Kudryavtsev <boblobl4@gmail.com>
2020-05-09 02:48:46 +0300

    test: jest-circus

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: "node",
-  transform: { "^.+\\.jsx?$": "babel-jest", "^.+\\.tsx?$": "ts-jest" },
+  testRunner: "jest-circus/runner",
+  transform: { "^.+\\.tsx?$": "ts-jest" },
   globals: { "ts-jest": { packageJson: "package.json" } },
   testMatch: ["<rootDir>/__tests__/**/*.(spec|test).[jt]s?(x)"],
   setupFilesAfterEnv: ["<rootDir>/__tests__/setup.ts"],
