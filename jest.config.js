e69b1c70eb7882231ea23cbe37d47f63faa64c87
Anton Kudryavtsev <boblobl4@gmail.com>
2020-05-12 02:05:34 +0300

    test: do not recurse inside __tests__

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   testRunner: "jest-circus/runner",
   transform: { "^.+\\.tsx?$": "ts-jest" },
   globals: { "ts-jest": { packageJson: "package.json" } },
-  testMatch: ["<rootDir>/__tests__/**/*.(spec|test).[jt]s?(x)"],
+  testMatch: ["<rootDir>/__tests__/*.(spec|test).[jt]s?(x)"],
   setupFilesAfterEnv: ["<rootDir>/__tests__/setup.ts"],
 };
