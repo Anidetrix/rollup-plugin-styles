49655a40479ff3e1a89f43cde8a88483c0209b90
Anton Kudryavtsev <boblobl4@gmail.com>
2020-05-13 20:45:47 +0300

    fix: uniform sass error message

--- src/loaders/sass/load.ts
+++ src/loaders/sass/load.ts
@@ -6,6 +6,15 @@ import loadModule from "../../utils/load-module";
 type SassImplementation = NonNullable<SASSLoaderOptions["impl"]>;
 const allSassIDs = ["node-sass", "sass"] as const;
 
+const idFmt = allSassIDs
+  .map((id, i, arr) => {
+    const newId = `\`${id}\``;
+    if (i === arr.length - 2) return newId;
+    if (i === arr.length - 1) return `or ${newId}`;
+    return `${newId},`;
+  })
+  .join(" ");
+
 export async function loadSass(impl?: SassImplementation): Promise<[Sass, SassImplementation]> {
   const sassIDs = impl ? ([impl] as const) : allSassIDs;
 
@@ -16,11 +25,5 @@ export async function loadSass(impl?: SassImplementation): Promise<[Sass, SassIm
     if (impl) throw new Error(`Could not load \`${impl}\` Sass implementation`);
   }
 
-  throw new Error(
-    [
-      "You need to install one of the following packages:",
-      allSassIDs.map(id => `\`${id}\``).join(", "),
-      "in order to process Sass files",
-    ].join("\n"),
-  );
+  throw new Error(`You need to install ${idFmt} package in order to process Sass files`);
 }
