fb31286eff12eca938c9ce30b7422e01fb9e0fea
Anton Kudryavtsev <boblobl4@gmail.com>
2020-05-20 23:24:03 +0300

    fix: better typedoc

--- src/loaders/postcss/modules/index.ts
+++ src/loaders/postcss/modules/index.ts
@@ -1,8 +1,8 @@
 import postcss from "postcss";
 import modulesValues from "postcss-modules-values";
-import localByDefault, { Options as LocalByDefaultOptions } from "postcss-modules-local-by-default";
-import extractImports, { Options as ExtractImportsOptions } from "postcss-modules-extract-imports";
-import modulesScope, { Options as ScopeOptions } from "postcss-modules-scope";
+import localByDefault from "postcss-modules-local-by-default";
+import extractImports from "postcss-modules-extract-imports";
+import modulesScope from "postcss-modules-scope";
 import generateScopedNameDefault from "./generate";
 
 /** Options for [CSS Modules](https://github.com/css-modules/css-modules) */
@@ -11,11 +11,11 @@ export interface ModulesOptions {
    * Default mode for classes
    * @default "local"
    */
-  mode?: LocalByDefaultOptions["mode"];
+  mode?: "local" | "global" | "pure";
   /** Fail on wrong order of composition */
-  failOnWrongOrder?: ExtractImportsOptions["failOnWrongOrder"];
+  failOnWrongOrder?: boolean;
   /** Export global classes */
-  exportGlobals?: ScopeOptions["exportGlobals"];
+  exportGlobals?: boolean;
   /**
    * Placeholder or function for scoped name generation.
    * Allowed blocks for placeholder:
@@ -25,7 +25,7 @@ export interface ModulesOptions {
    * - `[hash(:<num>)]`: A hash based on the name and content of the asset (with optional length).
    * @default "[name]_[local]__[hash:8]"
    */
-  generateScopedName?: string | ScopeOptions["generateScopedName"];
+  generateScopedName?: string | ((name: string, file: string, css: string) => string);
   /** Function for resulting replacements extraction */
   getReplacements?: (file: string, replacements: { [x: string]: string }, out?: string) => void;
 }
