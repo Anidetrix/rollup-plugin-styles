08ae382212d51fec54e45610c50f3c3896ed99e2
Anton Kudryavtsev <boblobl4@gmail.com>
2020-12-26 20:03:42 +0300

    fix(utils): async resolve types

--- src/utils/resolve-async.ts
+++ src/utils/resolve-async.ts
@@ -2,5 +2,6 @@ import resolveAsync, { AsyncOpts } from "resolve";
 
 export default async (id: string, options: AsyncOpts = {}): Promise<string> =>
   new Promise((resolve, reject) => {
-    resolveAsync(id, options, (err, res) => (err ? reject(err) : resolve(res)));
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- always present if there is no error
+    resolveAsync(id, options, (err, res) => (err ? reject(err) : resolve(res!)));
   });
