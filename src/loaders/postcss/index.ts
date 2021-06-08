01fc0a788a1c126365bc0cef9f68e3174a9f89a1
Lucien Peslier <lpeslier@meilleursagents.com>
2021-06-08 14:42:35 +0200

    fix(treeshakable): use es5 syntax for treeshakable object

--- src/loaders/postcss/index.ts
+++ src/loaders/postcss/index.ts
@@ -196,7 +196,7 @@ const loader: Loader<PostCSSLoaderOptions> = {
             getters += `get ${name}() { ${injectorCallOnce} return ${value}; },\n`;
           }
 
-          getters += `inject() { ${injectorCallOnce} },`;
+          getters += `inject: function inject() { ${injectorCallOnce} },`;
           output.push(`var ${modulesVarName} = {${getters}};`);
         }
       }
