3b97f571d75653f195fe4a97df673cade519b4f7
Rahul Gupta <cxres@users.noreply.github.com>
2021-08-16 18:31:09 +0530

    fix: position of named export in injected css
    All named export are now placed at the end of the injected css code.
    
    This solves the issue of css sourcemap not being passed on to the generateBundle hook due to the css being replaced by invalid js.

--- __tests__/fixtures/modules/subdir/composition2.css.d.ts
+++ __tests__/fixtures/modules/subdir/composition2.css.d.ts
@@ -1,5 +1,8 @@
-export var css: string;
+var css: string;
 interface ModulesExports {"compositioned":"composition2_compositioned"}
 interface ModulesExports {inject:()=>void}
 declare const modules_354770d7: ModulesExports;
 export default modules_354770d7;
+export {
+  css
+};
