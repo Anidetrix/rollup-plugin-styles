f72ae15414595baa5b56bcfe04a0ab9906b43bda
Westbrook Johnson <westbrook.johnson@gmail.com>
2022-01-12 23:26:32 -0500

    chore: update husky usage for current version

--- husky.config.js
+++
@@ -1,8 +0,0 @@
-const tasks = arr => arr.join(" && ");
-
-module.exports = {
-  hooks: {
-    "pre-commit": tasks(["pnpm t -- --no-cache", "lint-staged"]),
-    "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
-  },
-};
